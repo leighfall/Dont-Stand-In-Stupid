d51cbcd44e6353c0757f8c208500bb2939162b1a
Autumn <autumn.leigh.henderson@gmail.com>
2024-10-20 19:39:27 -0400

    Add scss

--- content-app/app.vue
+++ content-app/app.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
   <div>
     <NuxtPage />
   </div>
 </template>
+
+<style lang="scss">
+@use '@/assets/index.scss' as *;
+
+body {
+  font-family: $font-stack;
+}
+</style>
